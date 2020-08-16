005d69c013329f4c9af3dbdd534ff8ac25c73486
oleg <electrochange@mail.ru>
2020-08-16 10:37:53 +0300

    - create Gallery model, register in admin

--- image_picker/models.py
+++ image_picker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, db_index=True, primary_key=True)
+    dir_path = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Galleries"
+
+    def __str__(self):
+        return self.dir_path
