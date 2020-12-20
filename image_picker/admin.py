d64bee9330d0c2f1e375073146d1630c4beda8f8
des2048 <electrochange@mail.ru>
2020-12-20 23:33:21 +0300

    add WidgetAttrsMixin for overriding attrs in form fields widgets in admin form. Disable autocomplete in Gallery form in admin

--- image_picker/admin.py
+++ image_picker/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import Gallery
 
-admin.site.register(Gallery)
+class WidgetAttrsMixin:
+    widgets_attrs = {}
+    
+    def get_widgets_attrs(self):
+        return self.widgets_attrs
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        
+        widgets_attrs = self.get_widgets_attrs()
+        
+        for field, attrs in widgets_attrs.items():
+            form.base_fields[field].widget.attrs.update(attrs)
+        
+        return form
+    
+@admin.register(Gallery)
+class GalleryAdmin(WidgetAttrsMixin, admin.ModelAdmin):
+    widgets_attrs = {
+        'title': {'autocomplete' : 'off'},
+        'slug': {'autocomplete' : 'off'},
+        'dir_path': {'autocomplete' : 'off'}
+    }
+    
+
