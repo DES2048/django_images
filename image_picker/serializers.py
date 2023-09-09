7afe18e0fed7b40417e58eeddddd2a63d37db3e5
DES2048 <electrochange@mail.ru>
2023-09-09 19:55:16 +0300

    Validation for picker settings

--- image_picker/serializers.py
+++ image_picker/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Gallery
-from .services import PickerSettings
+from .services import PickerSettings, ShowMode
 
 
 class GallerySerializer(serializers.ModelSerializer):
@@ -11,8 +11,21 @@ class GallerySerializer(serializers.ModelSerializer):
 
 class SettingsSerializer(serializers.Serializer):
     selected_gallery = serializers.CharField(max_length=128)
-    show_mode = serializers.CharField(max_length=20, default="unmarked")
+    show_mode = serializers.CharField(max_length=20, default=ShowMode.UNMARKED)
 
+    def validate_selected_gallery(self, value):
+        try:
+            Gallery.objects.get(pk=value)
+        except Gallery.DoesNotExist:
+            raise serializers.ValidationError(f" gallery '{value}' doesn't exist")
+        return value
+    
+    def validate_show_mode(self, value):
+        if value not in ShowMode.MODES_LIST:
+            raise serializers.ValidationError("invalid show_mode value")
+        
+        return value
+    
     def save(self, **kwargs):
         settings = PickerSettings(
             self.validated_data['selected_gallery'],
